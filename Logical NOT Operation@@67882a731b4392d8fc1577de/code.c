924b3611394299750ab7028fd32a071532c7cf53
Chahat <chahatchawla44@gmail.com>
2025-02-26 00:31:38 +0530

    Solution to Logical NOT Operation

--- Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
+++ Logical NOT Operation@@67882a731b4392d8fc1577de/code.c
@@ -4,11 +4,6 @@ int main() {
     int num1;
 
     
-    if (scanf("%d", &num1) != 1) {
-        printf("Invalid input\n");
-        return 1;
-    }
-
  
     if (num1 <= 0) {
         printf("True\n"); 
